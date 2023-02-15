b314a10c73a2df49090695923e27d50837340773
Dalton Muck <dalton.muck22@icloud.com>
2023-02-15 16:10:42 -0500

    saving before I try something new

--- wordle.cc
+++ wordle.cc
@@ -10,101 +10,160 @@
 
 using namespace std;
 
-//const string TUBES = "tubes";
+const string TUBES = "tubes";
 int main()
 {
-	// string input;
-	// int first = 0;
-	// int secound = 0;
-	// int third = 0;
-	// int fourth = 0;
-	// int fifth = 0;
+	string input;
+	int first = 0;
+	int second = 0;
+	int third = 0;
+	int fourth = 0;
+	int fifth = 0;
 
-	// string a = " ";
-	// string b = " ";
-	// string c = " ";
-	// string d = " ";
-	// string e = " ";
+	string a = " ";
+	string b = " ";
+	string c = " ";
+	string d = " ";
+	string e = " ";
+	int n = 0;
+	while (input != TUBES && n != 5)
+	{
+		// prompt for guess
+		cout << "please enter your next guess" << endl;
+		cin >> input;
+		// check for yellow
+		if (input.at(n) == TUBES.at(0) || input.at(n) == TUBES.at(1) || input.at(n) == TUBES.at(2) || input.at(n) == TUBES.at(3) || input.at(n) == TUBES.at(4))
+		{
+			second = 1;
+		}
+		else if (input.at(n) == TUBES.at(2))
+		{
+			third = 1;
+		}
+		else if (input.at(n) == TUBES.at(3))
+		{
+			fourth = 1;
+		}
+		else if (input.at(n) == TUBES.at(4))
+		{
+			fifth = 1;
+		}
 
-	// for (int n = 0; input != TUBES; n++)
-	// {
-	// 	// prompt for guess
-	// 	cout << "please enter your next guess" << endl;
-	// 	cin >> input;
-	// 	// check for yellow
-	// 	if (input.at(n) == input.at(1))
-	// 	{
-	// 		secound = 1;
-	// 	}
-	// 	else if (input.at(n) == input.at(2))
-	// 	{
-	// 		third = 1;
-	// 	}
-	// 	else if (input.at(n) == input.at(3))
-	// 	{
-	// 		fourth = 1;
-	// 	}
-	// 	else if (input.at(n) == input.at(4))
-	// 	{
-	// 		fifth = 1;
-	// 	}
+		// check for green
+		if (input.at(n) == TUBES.at(n))
+		{
+			if (n == 0)
+			{
+				first = 2;
+			}
+			else if (n == 1)
+			{
+				second = 2;
+			}
+			else if (n == 2)
+			{
+				third = 2;
+			}
+			else if (n == 3)
+			{
+				fourth = 2;
+			}
+			else if (n == 4)
+			{
+				fifth = 2;
+			}
+		}
+					// print green, yellow, or white for first char
+			if (first == 1)
+			{
+				a = input.at(0);
+				cout << "\033[1;33m" + a + "\033[0m";
+			}
+			else if (first == 2)
+			{
+				a = input.at(0);
+				cout << "\033[1;32m" + a + "\033[0m";
+			}
+			else
+			{
+				cout << input.at(0);
+			}
 
-	// 	// check for green
-	// 	if (input.at(n) == TUBES.at(n))
-	// 	{
-	// 		if (n == 0)
-	// 		{
-	// 			first = 2;
-	// 		}
-	// 		else if (n == 1)
-	// 		{
-	// 			secound = 2;
-	// 		}
-	// 		else if (n == 2)
-	// 		{
-	// 			third = 2;
-	// 		}
-	// 		else if (n == 3)
-	// 		{
-	// 			fourth = 2;
-	// 		}
-	// 		else if (n == 4)
-	// 		{
-	// 			fifth = 2;
-	// 		}
-	// 	}
+			// second char
+			if (second == 1)
+			{
+				b = input.at(1);
+				cout << "\033[1;33m" + b + "\033[0m";
+			}
+			else if (second == 2)
+			{
+				b = input.at(1);
+				cout << "\033[1;32m" + b + "\033[0m";
+			}
+			else
+			{
+				cout << input.at(1);
+			}
 
+			// third char
+			if (third == 1)
+			{
+				c = input.at(2);
+				cout << "\033[1;33m" + c + "\033[0m";
+			}
+			else if (third == 2)
+			{
+				c = input.at(2);
+				cout << "\033[1;32m" + c + "\033[0m";
+			}
+			else
+			{
+				cout << input.at(2);
+			}
 
+			// fourth char
+			if (fourth == 1)
+			{
+				d = input.at(3);
+				cout << "\033[1;32m" + d + "\033[0m";
+			}
+			else if (first == 2)
+			{
+				d = input.at(3);
+				cout << "\033[1;33m" + d + "\033[0m";
+			}
+			else
+			{
+				cout << input.at(3);
+			}
 
-	// 	//print green letters
-	// 	if (first == 1)
-	// 	{
-	// 		a = input.at(n);
-	// 		a = "/033[1;32" + a + "\033";
-	// 		cout << a;
-	// 	}
-	// 	else if (secound == 1)
-	// 	{
-	// 		b = input.at(n);
-	// 		b = "/033[1;32" + b + "\033";
-	// 		cout << b;
-	// 	}
-	// 	else if (third == 1)
-	// 	{
+			// fifth char
+			if (fifth == 1)
+			{
+				e = input.at(4);
+				cout << "\033[1;32m" + e + "\033[0m";
+			}
+			else if (first == 2)
+			{
+				e = input.at(4);
+				cout << "\033[1;33m" + e + "\033[0m";
+			}
+			else
+			{
+				cout << input.at(5);
+			}
+			cout << " " << endl;
+		
+		n++;
 
-	// 	}
+	}
+		
 
-	// 	if (n == 6)
-	// 	{
-	// 		break;
-	// 	}
-	// }
-
-	string hi;
-	string a;
-	cin >> hi;
-	a = hi.at(0);
-	cout << "\033[1;32m" + a + "\033[0m" << endl;
-
+	// string hi;
+	// string a;
+	// cin >> hi;
+	// a = hi.at(0);
+	// cout << "\033[1;32m" + a + "\033[0m" << endl;
+	cout << " " << endl;
 	return 0;
 }
